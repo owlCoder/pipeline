0c469092ac2deaa573c51c537d062ddd94168ff2
Данијел Јовановић <danijel.xda@gmail.com>
2021-02-04 00:18:53 +0100

    www

---
+++ precache-manifest.42361d42982257802cf1ddafac0898c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eb0079e33da4c974d0bd264326f5f9d",
+    "url": "/owlcoder-pipeline/index.html"
+  },
+  {
+    "revision": "fe6b3e4d92c215f10cfe",
+    "url": "/owlcoder-pipeline/static/css/2.d64b8b57.chunk.css"
+  },
+  {
+    "revision": "fe6b3e4d92c215f10cfe",
+    "url": "/owlcoder-pipeline/static/js/2.9583270a.chunk.js"
+  },
+  {
+    "revision": "5953732d45f765683f7e",
+    "url": "/owlcoder-pipeline/static/js/main.0396bd16.chunk.js"
+  },
+  {
+    "revision": "50558b5c19c5cce40ec0",
+    "url": "/owlcoder-pipeline/static/js/runtime~main.627a50af.js"
+  }
+]);
